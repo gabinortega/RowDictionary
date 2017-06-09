55d035679b1f7936dba3ab61e4261be5897f2483
gabinortega <gabinortega@gmail.com>
2017-06-09 17:39:39 -0400

    Add add and trytoget functionality

---
+++ RowDictionary/RowDictionary.Tests/WhenAddingKeyAndValuesShould.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace RowDictionary.Tests
+{
+    [TestFixture]
+    class WhenAddingKeyAndValuesShould
+    {
+        [Test]
+        public void ShouldBeAbleToAddANewValueAndRetriveTheSameValueUsingTheSameKey()
+        {
+            var key = "key";
+            var value = "Hola mundo";
+            var dict = new RowDictionary<string, string>();
+            dict.Add(key, value);
+            string result;
+            dict.TryGetValue(key, out result);
+
+            Assert.That(result, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void ShouldBeAbleToRetrieveTheValueUsingTheKeyWhenThereIsOnlyOneValueInserted()
+        {
+            var key = 01;
+            var valueExpected = "valueExpected";
+            var dict = new RowDictionary<int, string>();
+            dict.Add(key, valueExpected);
+            string result;
+            dict.TryGetValue(key, out result);
+
+            Assert.That(result, Is.EqualTo(valueExpected));
+        }
+
+        [Test]
+        public void ShouldBeAbleToRetrieveTheValueUsingTheKeyWhenThereIsMoreThanOneValueInserted()
+        {
+            var key = 04;
+            var valueExpected = "valueExpected";
+            var dict = new RowDictionary<int, string>();
+            dict.Add(01, "01");
+            dict.Add(02, "02");
+            dict.Add(03, "03");
+            dict.Add(key, valueExpected);
+            string result;
+            dict.TryGetValue(key, out result);
+
+            Assert.That(result, Is.EqualTo(valueExpected));
+        }
+
+        [Test]
+        public void ShouldBeAbleToRetrieveTheValueUsingTheKeyWhenTheKeyIsNonPrimitive()
+        {
+            var myKey = new { FirstName = "ABC", LastName = "DFG" };
+            const string whenTheKeyIsAnObject = "WhenTheKeyIsAnObject";
+            var dict = new RowDictionary<object, string>();
+            dict.Add(myKey, whenTheKeyIsAnObject);
+            string result;
+            dict.TryGetValue(myKey, out result);
+
+            Assert.That(result, Is.EqualTo(whenTheKeyIsAnObject));
+        }
+    }
+}
--- RowDictionary/RowDictionary/RowDictionary.cs
+++ RowDictionary/RowDictionary/RowDictionary.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RowDictionary
 {
     public class RowDictionary<TKey, TValue>
     {
+        public List<KeyValuePair<TKey, TValue>> Row { get; set; }
+
+        public RowDictionary()
+        {
+            Row = new List<KeyValuePair<TKey, TValue>>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Row.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            value = default(TValue);
+            if (!Row.Any(x => x.Key.Equals(key))) return false;
+            value = Row.FirstOrDefault(x => x.Key.Equals(key)).Value;
+            return true;
+        }
     }
 }
